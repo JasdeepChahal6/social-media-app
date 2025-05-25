0f637bec00c5c4e4ef7a11220adc420878ad9ebf
JasdeepChahal6 <jasdeep20045@gmail.com>
2025-05-25 15:17:54 -0400

    port

--- server/index.js
+++ server/index.js
@@ -26,6 +26,7 @@ app.use(express.json());
 // });
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
